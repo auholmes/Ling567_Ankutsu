e2d3aa2c9649b17fd77e06f6fb8defd2816aad8b
UbuntuLKB User <auholmes@uw.edu>
2018-02-08 19:17:33 -0800

    fixed adjectives in testsuite

--- aqz/tsdb/skeletons/Index.lisp
+++ aqz/tsdb/skeletons/Index.lisp
@@ -21,6 +21,7 @@
 
 (
 ((:path . "matrix") (:content . "matrix: A test suite created automatically from the test sentences given in the Grammar Matrix questionnaire."))
+((:path . "lab5") (:content . "lab5 testsuite"))
 ; New test suites here. For example:
 ; ((:path . "new-test-suite") (:content . "New Test Suite: A description of the new test suite located at the subdirectory new-test-suite."))
 )
